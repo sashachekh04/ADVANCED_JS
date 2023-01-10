bc33195735335c2bd56347547b0848a66652eab5
Sasha Chekh <cheh170804@gmail.com>
2023-01-10 12:33:28 +0200

    feat: learn Object accsessors props

--- OBJ_DESCRIPTOR/index.js
+++ OBJ_DESCRIPTOR/index.js
@@ -1,8 +1,43 @@
+"use strict";
 const human = {
   name: "Sasha",
+  surname: "Chekh",
   age: 18,
   weight: 85,
-  surname: "Chekh",
+  sayHello() {
+    console.log(`Hello, I am ${this.name}`);
+  },
 };
 
-console.log(Object.getOwnPropertyDescriptors(human));
+function makePrivateProp(prop, obj) {
+  Object.defineProperty(obj, prop, {
+    configurable: false,
+    enumerable: false,
+  });
+}
+
+function hideObjectMethods(obj) {
+  for (let key in obj) {
+    if (obj[key] instanceof Function) {
+      console.log(true);
+      Object.defineProperty(obj, key, {
+        enumerable: false,
+      });
+    }
+  }
+}
+makePrivateProp("name", human);
+
+//ACCSESSORS (No value, writable, has configurable, enuumerable)
+const man = {};
+Object.defineProperty(man, "name", {
+  set(value) {
+    if (value.length < 4) {
+      throw new Error("Name should be longer than 4 symbols");
+    }
+    this._name = value;
+  },
+  get() {
+    return this._name;
+  },
+});
